26f6ecdec1c2da32ba45b76b0e270d68fc4d9c43
David Raygoza <david.raygoza.ramirez@gmail.com>
2018-12-22 01:17:31 -0600

    sample 01

---
+++ react_01/script_01.js
@@ -0,0 +1,12 @@
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <h1>Hello World</h1>
+    )
+  }
+});
+
+ReactDOM.render(
+  <HelloWorld/>,
+  document.getElementById('root')
+);
